71643ebbfc33212edd6e41d6d2c2a3e0ea4795a8
mougel <gaetan@familymougel.eu>
2015-03-27 20:37:22 +0000

    tiny changes to basket, order improved drastically

--- new_order.php
+++ new_order.php
@@ -1,31 +1,56 @@
 <?php session_start(); 
-	require_once("db_config.php");
-    // Connect to the Database and Select the tts database.
+require_once("db_config.php");
+// Connect to the Database and Select the tts database.
 
-	
-	$product_id = $_POST['product_id'];
-    $quantity = 1;
-	$missing = 0;
+if (!isset($_SESSION['basket'])) {
+    $_SESSION['basket'] = array();
+}//create the basket if it doesnt exist.
 
-	$question = 'SELECT availability FROM itemIngredients LEFT JOIN Ingredients WHERE itemIngredients.idItem = :id';
-	$sth = $db->prepare($question, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-	$sth->execute(array(':id' => $product_id));
-	$fetch = $sth->fetchAll();
+$max=count($_SESSION['basket']); //check how many items are in the basket.
 
-    $i=1;
+$total = 0;
+for($i=0;$i<$max;$i++){
+    $product_id = $_SESSION['basket'][$i]['product_id'];
+    $quantity = $_SESSION['basket'][$i]['quantity'];
+
+    //gets a product to order (one by one)
+
+    $missing = 0;
+
+    $question = 'SELECT availability FROM itemIngredients LEFT JOIN Ingredients WHERE itemIngredients.idItem = :id';
+    $sth = $db->prepare($question, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute(array(':id' => $product_id));
+    $fetch = $sth->fetchAll();
+
+    $x=1;
     foreach ($fetch as $key) {
-    	$availability[$i] = $key['availability'];
-    	if ($availability[$i]<1){
-    		$missing++;
-    	}
-    	$i++;
+        $availability[$x] = $key['availability'];
+        if ($availability[$x]<$quantity){
+            $missing++;
+        }
+        $x++;
     }
+    //checks if the stock is sufficient to place an order
 
     if ($missing > 0){
-    	$_SESSION['message'] = "6"; //Product Unavailable.
-		header('Location: '. $_POST['returnto']);
-		die();
+        $_SESSION['message'] = "6"; //Product Unavailable.
+        echo "<form action=removeBasket.php method=POST>
+                <input type='hidden' value='". $product_id ."' name='product_id' />
+                <input type='hidden' value='basket.php' name='returnto' />
+                <input type='hidden' value='". $availability[$i] ."' name='quantity'/>
+                <input type=submit name=DELETE/>
+                </form>";
+        ?>
+        <script type="text/javascript">
+        document.getElementById("DELETE").submit(); // automatically submits the form above.
+        </script>
+        <?php
+        die();
     }
+    //if not, go back to the basket, remove and declare one as missing stock. 
 
     
+
+}
+
 ?>
--- removeBasket.php
+++ removeBasket.php
@@ -3,6 +3,14 @@
 $product_id = $_POST['product_id'];
 $max=count($_SESSION['basket']);
 
+if isset($_POST['quantity']){
+    $q = $_POST['quantity'];
+}
+else {
+    $q = 1;
+}
+//this allows for the removal of more than one item (mainly used if a customer tries to order something where the stock is insuficient.)
+
 $flag=0;
 for($i=0;$i<$max;$i++){
     if($product_id==$_SESSION['basket'][$i]['product_id']){
@@ -19,7 +27,7 @@ if ($flag = 0){
 
 if ($_SESSION['basket'][$i]['quantity'] > 1){
 	$old_quantity = $_SESSION['basket'][$i]['quantity'];
-    $new_quantity = $old_quantity - 1;
+    $new_quantity = $old_quantity - $q;
     $_SESSION['basket'][$i]['quantity'] = $new_quantity;
 }
 else {
