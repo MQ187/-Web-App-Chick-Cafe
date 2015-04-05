96ec78ed4411ec313418b27567623946a946f2ac
mougel <gaetan@familymougel.eu>
2015-04-05 17:40:04 +0100

    DB update

--- DB.sql
+++ DB.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Apr 04, 2015 at 11:01 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2015 at 05:39 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `ccdb`
@@ -20,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `bank`
 --
 
-CREATE TABLE `bank` (
+CREATE TABLE IF NOT EXISTS `bank` (
 `idbank` int(11) NOT NULL,
   `idPayment` int(11) DEFAULT NULL,
   `bankAccount` varchar(17) NOT NULL,
@@ -34,7 +40,7 @@ CREATE TABLE `bank` (
 -- Table structure for table `card`
 --
 
-CREATE TABLE `card` (
+CREATE TABLE IF NOT EXISTS `card` (
 `idcard` int(11) NOT NULL,
   `idPayment` int(11) DEFAULT NULL,
   `card4` varchar(4) NOT NULL,
@@ -48,7 +54,7 @@ CREATE TABLE `card` (
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
+CREATE TABLE IF NOT EXISTS `customer` (
 `idCustomer` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
@@ -63,14 +69,13 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`idCustomer`, `name`, `surname`, `email`, `phone`, `password`, `isLoggedIn`) VALUES
-(3, 'ajadj', 'jajdaj', 'jajdajaj', 'jg', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 0),
 (4, 'Daniel', 'Muller', 'daniel', '7757766', '1f4129b2b73b8ce2b048846c93da6402608f12e6', 0),
 (5, 'naresh', 'chamkani', 'nausnu@hotmail.com', '4455667788', '0f869632dedf073cb0587e8dfa43ec94c872abfc', 0),
 (8, 'test', 'tester', 'test@gmail.com', '08977654322', 'cc03e747a6afbbcbf8be7668acfebee5', 0),
-(9, 'jim', 'bob', 'j@gmail.com', '123', 'adcd7048512e64b48da55b027577886ee5a36350', 0),
-(10, 'Gaetan', 'Mougel', 'gaetan@familymougel.eu', '07837324564', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 0),
+(10, 'Gaetan', 'Mougel', 'gaetan@familymougel.eu', '07837324564', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 1),
 (11, 'f', 'f', 'f@a', 'f', 'ec78ddda0cbcc3ba8f0e79ffc29e242cfccae579', 0),
-(12, 't', 't', 't', 't', 'b32f33e9fa28c36b11922d902038dc6c93200d8c', 0);
+(12, 't', 't', 't', 't', 'b32f33e9fa28c36b11922d902038dc6c93200d8c', 0),
+(22, 'jim', 'bob', 'j@gmail.com', '123', 'adcd7048512e64b48da55b027577886ee5a36350', 0);
 
 -- --------------------------------------------------------
 
@@ -78,7 +83,7 @@ INSERT INTO `customer` (`idCustomer`, `name`, `surname`, `email`, `phone`, `pass
 -- Table structure for table `customerdiscount`
 --
 
-CREATE TABLE `customerdiscount` (
+CREATE TABLE IF NOT EXISTS `customerdiscount` (
 `idcustomerDiscount` int(11) NOT NULL,
   `idcustomer` int(11) NOT NULL,
   `idDiscounts` int(11) NOT NULL
@@ -90,7 +95,7 @@ CREATE TABLE `customerdiscount` (
 -- Table structure for table `discounts`
 --
 
-CREATE TABLE `discounts` (
+CREATE TABLE IF NOT EXISTS `discounts` (
 `idDiscounts` int(11) NOT NULL,
   `vipMembership` int(11) NOT NULL,
   `discountType` tinyint(1) NOT NULL DEFAULT '0',
@@ -105,7 +110,7 @@ CREATE TABLE `discounts` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
+CREATE TABLE IF NOT EXISTS `employee` (
 `idemployee` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
@@ -127,7 +132,7 @@ INSERT INTO `employee` (`idemployee`, `name`, `password`, `email`, `active`) VAL
 -- Table structure for table `flexdiscount`
 --
 
-CREATE TABLE `flexdiscount` (
+CREATE TABLE IF NOT EXISTS `flexdiscount` (
   `idflexDiscount` int(11) NOT NULL,
   `idDiscount` int(11) DEFAULT NULL,
   `upper` decimal(6,2) NOT NULL,
@@ -141,7 +146,7 @@ CREATE TABLE `flexdiscount` (
 -- Table structure for table `ingredients`
 --
 
-CREATE TABLE `ingredients` (
+CREATE TABLE IF NOT EXISTS `ingredients` (
 `idIngredients` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `availability` int(11) NOT NULL DEFAULT '0',
@@ -153,9 +158,9 @@ CREATE TABLE `ingredients` (
 --
 
 INSERT INTO `ingredients` (`idIngredients`, `name`, `availability`, `price`) VALUES
-(1, 'Tomatoes', 786, 0.33),
-(2, 'letuce', 786, 0.33),
-(3, 'Cheddar', 20, 0.40);
+(1, 'Tomatoes', 786, '0.33'),
+(2, 'letuce', 786, '0.33'),
+(3, 'Cheddar', 20, '0.40');
 
 -- --------------------------------------------------------
 
@@ -163,7 +168,7 @@ INSERT INTO `ingredients` (`idIngredients`, `name`, `availability`, `price`) VAL
 -- Table structure for table `item`
 --
 
-CREATE TABLE `item` (
+CREATE TABLE IF NOT EXISTS `item` (
 `iditem` int(11) NOT NULL,
   `idMenu` int(11) DEFAULT NULL,
   `type` varchar(15) DEFAULT NULL,
@@ -172,59 +177,59 @@ CREATE TABLE `item` (
   `price` decimal(6,2) NOT NULL,
   `preperationTime` time NOT NULL,
   `dailySpecial` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`iditem`, `idMenu`, `type`, `name`, `description`, `price`, `preperationTime`, `dailySpecial`) VALUES
-(2, 1, 'Meal', 'Eggs Benedict', 'English muffins with English Ham', 10.00, '00:05:00', 0),
-(9, 2, 'Meal', 'Lunch Food', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', 5.00, '00:05:00', 0),
-(10, 1, 'Meal', 'Breakfast Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', 99.00, '00:05:00', 0),
-(11, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(12, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(13, 2, 'Meal', 'LUNCHCH Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(14, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(15, 1, 'Meal', 'Breakfast', 'Descrip', 99.00, '00:05:00', 0),
-(16, 1, 'Meal', 'Full English Breakfast', 'The full package', 28.00, '00:10:00', 0),
-(17, 1, 'Meal', 'Full', 'The', 28.00, '00:10:00', 0),
-(20, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(21, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(22, 2, 'Meal', 'Lunch Food', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', 5.00, '00:05:00', 0),
-(23, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', 5.00, '00:05:00', 0),
-(24, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', 5.00, '00:05:00', 0),
-(25, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', 5.00, '00:05:00', 0),
-(26, 2, 'Meal', 'LUNCHCH Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(27, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(28, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', 12.00, '00:05:00', 0),
-(29, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(30, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(31, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(32, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(33, 3, 'Meal', 'Dinner Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', 12.00, '00:05:00', 0),
-(34, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(35, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(36, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(37, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(38, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', 12.00, '00:05:00', 0),
-(39, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(40, 4, 'Drink', 'Drink Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', 12.00, '00:05:00', 0),
-(41, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(43, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(44, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(45, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(46, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(47, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(48, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(49, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(50, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(51, 4, 'Drink', 'Drink Test', 'Descrip for drink test', 12.00, '00:05:00', 0),
-(52, 1, 'Meal', 'test', 'tedst', 234.00, '00:10:00', 1),
-(53, 1, 'Meal', 'rtr', 'treter', 0.00, '00:00:00', 1),
-(54, 1, 'Meal', 'rtr', 'treter', 0.00, '00:00:00', 1),
-(55, 1, 'Meal', 'gtg', 'gtg', 21.00, '00:00:00', 1),
-(56, 1, 'Meal', 'gtg', 'gtg', 21.00, '00:00:00', 1);
+(2, 1, 'Meal', 'Eggs Benedict', 'English muffins with English Ham', '10.00', '00:05:00', 0),
+(9, 2, 'Meal', 'Lunch Food', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', '5.00', '00:05:00', 0),
+(10, 1, 'Meal', 'Breakfast Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', '99.00', '00:05:00', 0),
+(11, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(12, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(13, 2, 'Meal', 'LUNCHCH Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(14, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(15, 1, 'Meal', 'Breakfast', 'Descrip', '99.00', '00:05:00', 0),
+(16, 1, 'Meal', 'Full English Breakfast', 'The full package', '28.00', '00:10:00', 0),
+(17, 1, 'Meal', 'Full', 'The', '28.00', '00:10:00', 0),
+(20, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(21, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(22, 2, 'Meal', 'Lunch Food', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', '5.00', '00:05:00', 0),
+(23, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', '5.00', '00:05:00', 0),
+(24, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', '5.00', '00:05:00', 0),
+(25, 2, 'Meal', 'Lunch Food', 'Descrip for lunch test', '5.00', '00:05:00', 0),
+(26, 2, 'Meal', 'LUNCHCH Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(27, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(28, 2, 'Meal', '1 more Test', 'Descrip foewfr drink test', '12.00', '00:05:00', 0),
+(29, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(30, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(31, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(32, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(33, 3, 'Meal', 'Dinner Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', '12.00', '00:05:00', 0),
+(34, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(35, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(36, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(37, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(38, 3, 'Meal', 'Dinner Test', 'Descrip for dinner test', '12.00', '00:05:00', 0),
+(39, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(40, 4, 'Drink', 'Drink Test', 'TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST', '12.00', '00:05:00', 0),
+(41, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(43, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(44, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(45, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(46, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(47, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(48, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(49, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(50, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(51, 4, 'Drink', 'Drink Test', 'Descrip for drink test', '12.00', '00:05:00', 0),
+(52, 1, 'Meal', 'test', 'tedst', '234.00', '00:10:00', 1),
+(53, 1, 'Meal', 'rtr', 'treter', '0.00', '00:00:00', 1),
+(54, 1, 'Meal', 'rtr', 'treter', '0.00', '00:00:00', 1),
+(55, 1, 'Meal', 'gtg', 'gtg', '21.00', '00:00:00', 1),
+(56, 1, 'Meal', 'gtg', 'gtg', '21.00', '00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -232,12 +237,12 @@ INSERT INTO `item` (`iditem`, `idMenu`, `type`, `name`, `description`, `price`, 
 -- Table structure for table `itemingredients`
 --
 
-CREATE TABLE `itemingredients` (
+CREATE TABLE IF NOT EXISTS `itemingredients` (
 `iditemIngredients` int(11) NOT NULL,
   `idItem` int(11) NOT NULL,
   `idIngredients` int(11) NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `itemingredients`
@@ -252,14 +257,14 @@ INSERT INTO `itemingredients` (`iditemIngredients`, `idItem`, `idIngredients`, `
 -- Table structure for table `manager`
 --
 
-CREATE TABLE `manager` (
+CREATE TABLE IF NOT EXISTS `manager` (
 `idmanager` int(11) NOT NULL,
   `owner` tinyint(1) NOT NULL DEFAULT '0',
   `name` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `manager`
@@ -268,7 +273,8 @@ CREATE TABLE `manager` (
 INSERT INTO `manager` (`idmanager`, `owner`, `name`, `password`, `email`, `active`) VALUES
 (1, 0, 'jackie', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'jackie@chan.com', 1),
 (2, 1, 'Bruce', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'bruce@banner.hu', 1),
-(3, 0, 'man', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'man@m.com', 1);
+(3, 0, 'man', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'man@m.com', 0),
+(4, 1, 'Homer Simpsons', '63982e54a7aeb0d89910475ba6dbd3ca6dd4e5a1', 'homer@simpsons.com', 1);
 
 -- --------------------------------------------------------
 
@@ -276,7 +282,7 @@ INSERT INTO `manager` (`idmanager`, `owner`, `name`, `password`, `email`, `activ
 -- Table structure for table `menu`
 --
 
-CREATE TABLE `menu` (
+CREATE TABLE IF NOT EXISTS `menu` (
 `idmenu` int(11) NOT NULL,
   `menuType` varchar(45) NOT NULL,
   `openTime` time NOT NULL,
@@ -302,7 +308,7 @@ INSERT INTO `menu` (`idmenu`, `menuType`, `openTime`, `closeTime`) VALUES
 -- Table structure for table `order`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE IF NOT EXISTS `order` (
 `idorder` int(11) NOT NULL,
   `idCustomer` int(11) NOT NULL,
   `idEmployee` int(11) DEFAULT NULL,
@@ -319,7 +325,7 @@ CREATE TABLE `order` (
 --
 
 INSERT INTO `order` (`idorder`, `idCustomer`, `idEmployee`, `orderType`, `orderTimeS`, `orderPriority`, `orderStatus`, `etc`, `timeCompleted`) VALUES
-(22, 12, 2, 'Breakfast', '2015-04-04 18:50:31', 0, 'Completed', '00:00:00', '0000-00-00 00:00:00');
+(22, 10, 2, 'Breakfast', '2015-04-04 18:50:31', 0, 'Preparing', '00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -327,7 +333,7 @@ INSERT INTO `order` (`idorder`, `idCustomer`, `idEmployee`, `orderType`, `orderT
 -- Table structure for table `orderitem`
 --
 
-CREATE TABLE `orderitem` (
+CREATE TABLE IF NOT EXISTS `orderitem` (
 `idorderItem` int(11) NOT NULL,
   `idOrder` int(11) NOT NULL,
   `idItem` int(11) NOT NULL,
@@ -347,7 +353,7 @@ INSERT INTO `orderitem` (`idorderItem`, `idOrder`, `idItem`, `quantity`) VALUES
 -- Table structure for table `payment`
 --
 
-CREATE TABLE `payment` (
+CREATE TABLE IF NOT EXISTS `payment` (
 `idPayment` int(11) NOT NULL,
   `idCustomer` int(11) NOT NULL,
   `idOrder` int(11) NOT NULL,
@@ -356,7 +362,7 @@ CREATE TABLE `payment` (
   `sucessful` tinyint(1) NOT NULL,
   `ammount` decimal(6,2) NOT NULL,
   `ammountDiscounted` decimal(6,2) NOT NULL,
-  `date` datetime NOT NULL
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -365,7 +371,7 @@ CREATE TABLE `payment` (
 -- Table structure for table `refund`
 --
 
-CREATE TABLE `refund` (
+CREATE TABLE IF NOT EXISTS `refund` (
 `idRefund` int(11) NOT NULL,
   `idCustomer` int(11) NOT NULL,
   `idManager` int(11) NOT NULL,
@@ -382,7 +388,7 @@ CREATE TABLE `refund` (
 -- Table structure for table `reports`
 --
 
-CREATE TABLE `reports` (
+CREATE TABLE IF NOT EXISTS `reports` (
 `idreports` int(11) NOT NULL,
   `idmanager` int(11) NOT NULL,
   `date` datetime NOT NULL,
@@ -538,17 +544,17 @@ MODIFY `idIngredients` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=93;
+MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=57;
 --
 -- AUTO_INCREMENT for table `itemingredients`
 --
 ALTER TABLE `itemingredients`
-MODIFY `iditemIngredients` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `iditemIngredients` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `manager`
 --
 ALTER TABLE `manager`
-MODIFY `idmanager` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `idmanager` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `menu`
 --
@@ -656,3 +662,7 @@ ADD CONSTRAINT `refundOrder` FOREIGN KEY (`idOrder`) REFERENCES `order` (`idorde
 --
 ALTER TABLE `reports`
 ADD CONSTRAINT `reportsManager` FOREIGN KEY (`idmanager`) REFERENCES `manager` (`idmanager`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
